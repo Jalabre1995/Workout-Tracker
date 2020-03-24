ac7901a75e88e1ee53f66ca95a26053133c472c4
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-24 11:34:22 -0400

    Workout-Tracker

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000;
-
+const path = require('path');
+const logger = require('morgan');
 
 const app = express();
+
+app.use(logger('dev'));
+
+const PORT = process.env.PORT || 3000;
+const databaseUrl = 'exeercises';
+const collections = ['type', 'name', 'duration', 'weight', 'reps', 'sets', 'distance']
+
+const db = mongojs(databaseUrl, collections);
+
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker",{
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+
+db.on('error', function(error){
+console.log('Database Error:', error);
+})
 
 const db = require('./models')
-
-
-
 //routes
 app.use(require("./public/api.js"));
 
