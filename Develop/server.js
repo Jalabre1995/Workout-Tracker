58156affdb29e0aa5244b86aba779e8b13da5285
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-25 16:43:10 -0400

    Changes

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path');
 const logger = require('morgan');
+const mongojs = require('mongojs')
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+const db = require("./models")
+
 
 app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 const databaseUrl = 'exeercises';
@@ -14,17 +24,12 @@ const collections = ['type', 'name', 'duration', 'weight', 'reps', 'sets', 'dist
 const db = mongojs(databaseUrl, collections);
 
 
-app.use(express.urlencoded({ extended: true}));
-app.use(express.json());
-
-app.use(express.static("public"));
-
 
 db.on('error', function(error){
 console.log('Database Error:', error);
 })
 
-const db = require('./models')
+const User = require('./models/tracker')
 //routes
 app.use(require("./public/api.js"));
 
