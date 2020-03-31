2ef0c394863c3d8e159f7ceba3b63bb1387116f1
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-31 17:20:25 -0400

    Mongo DB conncected to Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -20,8 +20,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://workout:password23@ds225253.mlab.com:25253/heroku_9wwtcqtb";
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://workout:password23@ds225253.mlab.com:25253/heroku_9wwtcqtb", { useNewUrlParser: true});
+mongoose.connect(MONGODB_URI); 
 //Local Host 'mongod://localhost/workout'
 
 
